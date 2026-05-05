2edef007d697adf2ea4f74c8753c2bf5510667ce
CHAKRAbdedaym <chkradobl@gmail.com>
2026-05-05 17:20:25 +0100

    Blueprint_Frontend

---
+++ Frontend/taskmanager-ui/src/app/tasks/tasks.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskService, Task } from '../services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.taskService.getTasks().subscribe(data => {
+      this.tasks = data;
+    });
+  }
+}
